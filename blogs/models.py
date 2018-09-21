0e94f66d0acb7da4475da03f83c8f08726587094
Unknown <antonio.daniswara@gmail.com>
2018-09-21 20:50:50 +1000

    Blog Model
    Add new fields (Post, Comment)
    Add the field attribute default value

--- blogs/models.py
+++ blogs/models.py
@@ -6,14 +6,25 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=150)
     detail = models.TextField()
-    score = models.IntegerField()
-    # creator = models.ForeignKey(User)
-    create_time = models.DateTimeField()
-    update_time = models.DateTimeField()
-    is_pinned = models.BooleanField()
+    score = models.IntegerField(default=0)
+    author = models.ForeignKey(
+        'auth.User', 
+        on_delete=models.CASCADE,
+        blank=True, 
+        null=True)
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, 
+        null=True)
+    is_pinned = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
     
     def was_published_recently(self):
         return self.create_time >= timezone.now() - datetime.timedelta(days=1)
@@ -21,9 +32,15 @@ class Post(models.Model):
 class Comment(models.Model):
     detail = models.TextField()
     score = models.IntegerField()
-    create_time = models.DateTimeField()
-    update_time = models.DateTimeField()
-    # creator = models.ForeignKey(User)
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        'auth.User', 
+        on_delete=models.CASCADE,
+        blank=True, 
+        null=True)
+
+
     def __str__(self):
         return self.detail
 
