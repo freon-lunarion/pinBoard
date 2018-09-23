0aa381fbbda1a2798f9166f0f26a497dc7f726ca
DeannaWang <mansongye@gmail.com>
2018-09-23 11:48:19 +1000

    Created Content model to be the base_class of other models to get rid of redundancy.

--- blogs/models.py
+++ blogs/models.py
@@ -1,20 +1,30 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.db.models import Sum
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=150)
+
+class Content(models.Model):
     detail = models.TextField()
-    
     author = models.ForeignKey(
-        'auth.User', 
+        'auth.User',
         on_delete=models.CASCADE)
     create_time = models.DateTimeField(default=timezone.now)
     update_time = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(
-        blank=True, 
+        blank=True,
         null=True)
+
+    @property
+    def score(self):
+        return Vote.objects.filter(content=self).aggregate(total=Sum("value"))["total"]
+
+    def is_published_recently(self):
+        return self.create_time >= timezone.now() - datetime.timedelta(days=1)
+
+class Post(Content):
+    title = models.CharField(max_length=150)
     is_pinned = models.BooleanField(default=False)
 
     def __str__(self):
@@ -24,53 +34,28 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
-    @property
-    def get_score(self):
-        return PostVote.objects.filter(post=self).aggregate(total=Sum("value"))["total"]
-    
-    def was_published_recently(self):
-        return self.create_time >= timezone.now() - datetime.timedelta(days=1)
-
-class Comment(models.Model):
-    detail = models.TextField()
-    
-    create_time = models.DateTimeField(default=timezone.now)
-    update_time = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(
-        'auth.User', 
-        on_delete=models.CASCADE)
-    post = models.ForeignKey(
+class Comment(Content):
+    parent = models.ForeignKey(
         Post, 
-        on_delete=models.CASCADE)
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.detail
-    
-    @property
-    def get_score(self):
-        return PostVote.objects.filter(post=self).aggregate(total=Sum("value"))["total"]
 
 class Tag(models.Model):
     title = models.CharField(max_length=50)
     def __str__(self):
         return self.title
 
-class PostTag(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+class Vote(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE)
+    value = models.IntegerField(default=1)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+class ContentTag(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
     tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
-
-class PostVote(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)   
-    author = models.ForeignKey(
-        'auth.User', 
-        on_delete=models.CASCADE)
-    value = models.IntegerField(default=1)
-    timestamp = models.DateTimeField(default=timezone.now)
-
-class CommentVote(models.Model):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)   
-    author = models.ForeignKey(
-        'auth.User', 
-        on_delete=models.CASCADE)
-    value = models.IntegerField(default=1)
-    timestamp = models.DateTimeField(default=timezone.now)
