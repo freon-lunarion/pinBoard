268319e65682031c4af876e9514fcc748f481977
DeannaWang <mansongye@gmail.com>
2018-09-25 20:54:36 +1000

    merge deanna

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from .models import Post, Comment, Vote, Content
-from django.contrib.auth.models import User
+from .models import Post, Comment
+from shared.models import *
 
 # Create your tests here.
 
@@ -10,8 +10,8 @@ class PostTestCase(TestCase):
         user = User.objects.create_user(username='admin', password='admin')
         p1 = Post.objects.create(title="liontitle", detail="lion", author=user)
         c1 = Comment.objects.create(parent=p1, detail="lion", author=user)
-        Vote.objects.create(content=p1, author=user, value=1)
-        Vote.objects.create(content=c1, author=user, value=-1)
+        Vote.objects.create(content=p1, user=user, value=1)
+        Vote.objects.create(content=c1, user=user, value=-1)
 
     def test_get_posts(self):
         post = Post.objects.get(id=1)
