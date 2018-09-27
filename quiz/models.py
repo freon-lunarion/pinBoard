16d11d2730cec7f5b37d2b5e60844df8d9db38de
Unknown <antonio.daniswara@gmail.com>
2018-09-27 17:43:52 +1000

    quiz module
    create model

---
+++ quiz/models.py
@@ -0,0 +1,33 @@
+import datetime
+from django.db import models
+from shared.models import *
+
+# Each Quiz is a question bank, which can be use for one theme,a course or a topic.
+class QuizBank(models.Model):
+    title = models.CharField(max_length=100)
+    detail = models.TextField(blank=True, null=True) # this for decription of the quiz or instruction
+
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # creator will have special previllge to edit the setting of this quiz
+
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(default=timezone.now)
+
+    # range time quiz (bank) accept the new question. Before and after that time, the antoher user cannot submit new questions
+    submit_beginTime = models.DateTimeField(default=timezone.now)
+    submit_endTime = models.DateTimeField(default=timezone.now)
+
+    # setting for displaying question
+    tryout_minScore = models.IntegerField(default=0) # only questions with score above this limit will be use in tryou session 
+    tryout_displayNum = models.IntegerField(default=5) # how many question displayed on tryout
+
+    # range time quiz (bank) can be use for tryout 
+    tryout_beginTime = models.DateTimeField(default=timezone.now)
+    tryout_endTime = models.DateTimeField(default=timezone.now)
+
+class Question(Content):
+    quizBank = models.ForeignKeyField(QuizBank, on_delete=models.CASCADE)
+    
+class Options(models.Model):
+    question = models.ForeignKeyField(Question, on_delete=models.CASCADE)
+    detail = models.TextField()
+    isCorrect = models.BooleanField(default=False)
