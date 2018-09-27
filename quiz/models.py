02d4d1c1fafd01cc20f5dcf37e79a0e3a6fac97b
Unknown <antonio.daniswara@gmail.com>
2018-09-27 18:16:12 +1000

    add method on QuizBank class
    is_submit_time() to check quiz open to submit new question 
    is_tryout_time() to check quiz open to tryout session

--- quiz/models.py
+++ quiz/models.py
@@ -24,6 +24,21 @@ class QuizBank(models.Model):
     tryout_beginTime = models.DateTimeField(default=timezone.now)
     tryout_endTime = models.DateTimeField(default=timezone.now)
 
+
+    def is_submit_time(self):
+        if (submit_beginTime <= timezone.now and timezone.now <= submit_endTime) {
+            return True
+        } else {
+            return False
+        }
+    
+    def is_tryout_time(self):
+        if (tryout_beginTime <= timezone.now and timezone.now <= tryout_endTime) {
+            return True
+        } else {
+            return False
+        }
+
 class Question(Content):
     quizBank = models.ForeignKeyField(QuizBank, on_delete=models.CASCADE)
     
