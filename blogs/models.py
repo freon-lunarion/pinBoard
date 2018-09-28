a16d558fd22065bda9422c25bb0d45e9af51ca94
Isaac Mbira <inm206@nyu.edu>
2018-09-28 17:12:33 +1000

    Fixes for blogs
    Updated model to support kind of post, i.e. Question, Image, Post as template was relying on title for that functionality. Also updated urls, view function and template so data is pulled into the page

--- blogs/models.py
+++ blogs/models.py
@@ -9,6 +9,7 @@ from django.shortcuts import get_object_or_404
 
 class Post(Content):
     title = models.CharField(max_length=150)
+    kind = models.CharField(max_length=20, default='Post')
     is_pinned = models.BooleanField(default=False)
     pin_board = models.ForeignKey(PinBoard, on_delete=models.CASCADE, default=None, blank=True, null=True)
     operator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
