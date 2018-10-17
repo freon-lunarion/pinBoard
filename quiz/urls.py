d5042beb247e10cfd14351c4841270cf0746b380
freon-lunarion <antonio.daniswara@gmail.com>
2018-10-18 07:56:57 +1100

    add: quizBank qoom creation (unfinised)

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 app_name = 'quiz'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.index_view, name='index'),
+    path('new', views.room_create, name='index'),
+    path('<int:pk>/', views.room_create, name='index'),
 ]
