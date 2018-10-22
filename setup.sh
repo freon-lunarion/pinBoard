82fd0a410047ec0bf8cd8a195eca8f67791962a0
freon-lunarion <antonio.daniswara@gmail.com>
2018-10-22 22:41:01 +1100

    edit: setup.sh remove virtualenv related command

--- setup.sh
+++ setup.sh
@@ -5,11 +5,9 @@ rm -r */migrations/__pycache__
 rm -r */migrations/0*.py
 rm db.sqlite3
 echo "db.sqlite3 deleted\n"
-virtualenv venv
-source venv/bin/activate
-pip3 install -r requirement.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py loaddata sample/users.json
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata sample/users.json
 echo "user001, user002, user003 and user004 added with 'abc123' as default password\n"
-python3 manage.py createsuperuser
+python manage.py createsuperuser
